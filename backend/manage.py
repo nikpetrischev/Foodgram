efe9b6c1fa0d3266bcd3a233456dd36a6d5ef7fd
Nikolai Petrishchev <nik.petrishchev@gmail.com>
2024-03-07 21:12:22 +0300

    Fix notPEP8 long string

--- backend/manage.py
+++ backend/manage.py
@@ -7,7 +7,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_backend.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'foodgram_backend.settings',
+    )
     try:
         # Django Library
         from django.core.management import execute_from_command_line
